7b4c44007d10ea3013dab278ca3d680646f8a167
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-02-20 23:58:20 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         for (int i = 1; i <= n; i++) {
             answer++;
-            while (((answer % 3) == 0) || ((answer / 10) == 13) || ((answer / 10) == 3) || ((answer % 10) == 3)) {
+            while (((answer % 3) == 0) || String.valueOf(answer).contains("3")) {
                 answer++;
             }
         }
