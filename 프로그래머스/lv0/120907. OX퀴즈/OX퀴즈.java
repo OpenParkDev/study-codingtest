19bbd28a185d24c3204535d70224415eb42f6389
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-02-21 22:41:57 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -3,25 +3,13 @@ class Solution {
         String[] answer = new String[quiz.length];
         for (int i = 0; i < quiz.length; i++) {
             String[] elem = quiz[i].split(" ");
-            int first = Integer.parseInt(elem[0]);
-            int second = Integer.parseInt(elem[2]);
-            int result = Integer.parseInt(elem[4]);
-            if (elem[1].equals("+")) {
-                if ((first + second) == result) {
-                    answer[i] = "O";
-                }
-                else {
-                    answer[i] = "X";
-                }
-            }
-            else {
-                if ((first - second) == result) {
-                    answer[i] = "O";
-                }
-                else {
-                    answer[i] = "X";
-                }
-            }
+            int input1 = Integer.parseInt(elem[0]);
+            int input2 = Integer.parseInt(elem[2]);
+            int output = Integer.parseInt(elem[4]);
+            
+            int real = (elem[1].equals("+")) ? (input1 + input2) : (input1 - input2);
+            String result = (real == output) ? "O" : "X";
+            answer[i] = result;
         }
         return answer;
     }
--- 프로그래머스/lv0/120907. OX퀴즈/README.md
+++ 프로그래머스/lv0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=java) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 0.09 ms
+메모리: 80.2 MB, 시간: 0.11 ms
 
 ### 구분
 
