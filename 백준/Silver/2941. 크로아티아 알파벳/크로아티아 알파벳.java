e1cffd92a3f3c45805cd6ab412a349236ac32f56
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-03-01 00:15:05 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 144 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -3,34 +3,25 @@ import java.io.*;
 public class Main {
     private static int numOfCrChar(String str) {
         int count = 0;
-        char gran = 0;
         char prev = 0;
         for (int i = 0; i < str.length(); i++) {
             char curr = str.charAt(i);
-            if (curr == '=') {
-                if ((prev == 's') || (prev == 'c')) {
-                    gran = 0;
-                    prev = 0;
-                }
-                else if (gran == 'd') {
+            if ((curr == '=') || (curr == '-')) {
+                if ((curr == '=') && (prev == '!')) {
                     count--;
-                    gran = 0;
-                    prev = 0;
                 }
-            }
-            else if (curr == '-') {
-                gran = 0;
-                prev = 0;
-            }
-            else if ((curr == 'j') && ((prev == 'l') || (prev == 'n'))) {
-                gran = 0;
-                prev = 0;
             }
             else {
-                count++;
-                gran = prev;
-                prev = curr;
+                if ((curr == 'z') && (prev == 'd')) {
+                    prev = '!';
+                    count++;
+                    continue;
+                }
+                else if (!((curr == 'j') && ((prev == 'l') || (prev == 'n')))) {
+                    count++;
+                }
             }
+            prev = curr;
         }
 
         return count;
