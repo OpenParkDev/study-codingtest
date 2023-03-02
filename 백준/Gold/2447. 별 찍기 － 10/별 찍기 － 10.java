32551d17be8b5bda48eda52bd89fb4d5ae78e2d0
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-03-03 02:08:36 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 280 ms, Memory: 25056 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -2,27 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static void fillBlank(String[][] mat) {
-        for (String[] row : mat) {
-            Arrays.fill(row, " ");
+    private static char[][] matrix;
+
+    private static void fillBlank() {
+        for (char[] row : matrix) {
+            Arrays.fill(row, ' ');
         }
     }
 
-    private static void fillSquare(String[][] mat, int x, int y, int size) {
-        if (size == 3) {
-            for (int i = x; i < x+size; i += size/3) {
-                for (int j = y; j < y+size; j += size/3) {
-                    if ((i != x+size/3) || (j != y+size/3)) {
-                        mat[i][j] = "*";
-                    }
-                }
-            }
+    private static void fillStar(int x, int y, int size) {
+        if (size == 1) {
+            matrix[x][y] = '*';
             return;
         }
         for (int i = x; i < x+size; i += size/3) {
             for (int j = y; j < y+size; j += size/3) {
                 if ((i != x+size/3) || (j != y+size/3)) {
-                    fillSquare(mat, i, j, size/3);
+                    fillStar(i, j, size/3);
                 }
             }
         }
@@ -32,15 +28,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
 
-        String[][] result = new String[size][size];
-        fillBlank(result);
-        fillSquare(result, 0, 0, size);
+        matrix = new char[size][size];
+        fillBlank();
+        fillStar(0, 0, size);
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (String[] row : result) {
-            for (String block : row) {
-                bw.write(block);
-            }
+        for (char[] row : matrix) {
+            bw.write(row);
             bw.write("\n");
         }
         bw.flush();
