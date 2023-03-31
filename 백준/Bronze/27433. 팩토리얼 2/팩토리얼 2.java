bf59f72ba84a3bd2683be0c1914dec3b5a2c92f4
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-03-31 23:21:51 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    private static long getFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n*getFactorial(n-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(getFactorial(n)));
+        bw.flush();
+        bw.close();
+    }
+}
