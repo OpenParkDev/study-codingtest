f9cda3632a8d2eb12f2dd1000e6faa055e59a34b
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-04-06 23:31:27 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+   public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(n * (n-1)));
+        bw.flush();
+        bw.close();
+    }
+}
