f45ba5269f6bb65d10f7a4d629437f47b0c875ef
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-04-11 15:36:56 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 53544 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+        while (queue.size() > 1) {
+            queue.poll();
+            if (queue.size() == 1) {
+                break;
+            }
+            else {
+                int i = queue.poll();
+                queue.add(i);
+            }
+        }
+        int result = queue.poll();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}
