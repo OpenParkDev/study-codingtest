4e0e1645cfca26213bdbbb4740a1147edea0be95
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-04-16 23:16:42 +0900

    [Silver III] Title: N과 M (1), Time: 264 ms, Memory: 42156 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,14 +2,22 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static String getSeries(int n, int m, String prev) {
-        if (m == 0) {
-            return prev + "\n";
+    private static char[] series;
+    private static boolean[] isChecked;
+
+    private static String getSeries(int n, int m, int idx) {
+        if (idx == m) {
+            series[2*m - 1] = '\n';
+            return String.valueOf(series);
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            if (!prev.contains(Integer.toString(i))) {
-                sb.append(getSeries(n, m-1, prev + i + " "));
+            if (!isChecked[i]) {
+                isChecked[i] = true;
+                series[2*idx] = (char) (i + '0');
+                series[2*idx + 1] = ' ';
+                sb.append(getSeries(n, m, idx+1));
+                isChecked[i] = false;
             }
         }
         return sb.toString();
@@ -20,9 +28,11 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        series = new char[2*m];
+        isChecked = new boolean[n+1];
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(getSeries(n, m, ""));
+        bw.write(getSeries(n, m, 0));
         bw.flush();
         bw.close();
     }
