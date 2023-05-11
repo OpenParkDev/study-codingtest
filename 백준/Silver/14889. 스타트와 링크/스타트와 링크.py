03df618a7c547bb7164a6e24022c3772e540b30f
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-11 16:16:31 +0900

    [Silver II] Title: 스타트와 링크, Time: 120 ms, Memory: 56392 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,25 +1,23 @@
 from itertools import combinations
 
-def read():
+def read_input():
     team_size = int(input())
-    stat_matrix = list()
-    for i in range(team_size):
-        stat_matrix.append(list(map(int, input().split())))
+    stat_matrix = [list(map(int, input().split())) for i in range(team_size)]
     return [team_size, stat_matrix]
 
 def get_min_diff():
-    team_size, stat_matrix = read()
+    team_size, stat_matrix = read_input()
     
     row_sum = [sum(i) for i in stat_matrix]
     col_sum = [sum(j) for j in zip(*stat_matrix)]
     stats = [i+j for i, j in zip(row_sum, col_sum)]
     
-    total_stat = sum(stats)
+    total_stat = sum(row_sum)
     cases = list(combinations(stats, team_size // 2))
     
     min_diff = total_stat
     for case in cases:
-        curr_diff = abs(total_stat // 2 - sum(case))
+        curr_diff = abs(total_stat - sum(case))
         if min_diff > curr_diff:
             min_diff = curr_diff
     
