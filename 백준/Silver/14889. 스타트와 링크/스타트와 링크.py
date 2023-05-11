8df7325cc7b648014a0fee5f422c062fa4e9028f
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-11 16:07:46 +0900

    [Silver II] Title: 스타트와 링크, Time: 172 ms, Memory: 56392 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,34 +1,28 @@
-import sys
+from itertools import combinations
 
-matrix = []
-minDiff = sys.maxsize
+def read():
+    team_size = int(input())
+    stat_matrix = list()
+    for i in range(team_size):
+        stat_matrix.append(list(map(int, input().split())))
+    return [team_size, stat_matrix]
 
-def getDiff(list, end):
-    team1 = 0
-    for i in range(0, len(list)):
-        for j in range(i+1, len(list)):
-            team1 += matrix[list[i]][list[j]] + matrix[list[j]][list[i]]
-    list2 = [i for i in range(end) if i not in list]
-    team2 = 0
-    for i in range(0, len(list2)):
-        for j in range(i+1, len(list2)):
-            team2 += matrix[list2[i]][list2[j]] + matrix[list2[j]][list2[i]]
-    return abs(team1 - team2)
+def get_min_diff():
+    team_size, stat_matrix = read()
+    
+    row_sum = [sum(i) for i in stat_matrix]
+    col_sum = [sum(i) for i in zip(*stat_matrix)]
+    stats = [row_sum[i] + col_sum[i] for i in range(team_size)]
+    
+    total_stat = sum(stats)
+    cases = list(combinations(stats, team_size // 2))
+    
+    min_diff = total_stat
+    for case in cases:
+        curr_diff = abs(total_stat // 2 - sum(case))
+        if min_diff > curr_diff:
+            min_diff = curr_diff
+    
+    print(min_diff)
 
-def setTeam(list, start, end, remain):
-    if remain == 0:
-        curDiff = getDiff(list, end)
-        global minDiff
-        if minDiff > curDiff:
-            minDiff = curDiff
-        return
-    for i in range(start, end):
-        list.append(i)
-        setTeam(list, i+1, end, remain-1)
-        list.pop()
-
-teamSize = int(input())
-for i in range(teamSize):
-    matrix.append(list(map(int, input().split())))
-setTeam(list(), 0, teamSize, teamSize/2)
-print(minDiff)
+get_min_diff()
