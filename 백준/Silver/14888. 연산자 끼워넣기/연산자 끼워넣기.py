29a9a65e37b90dad14b7f1920e85ef72f493bc34
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-15 15:11:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -2,12 +2,18 @@ import sys
 
 max = -1e9
 min = 1e9
+nums_list = []
 
 def read_input():
     nums_size = int(sys.stdin.readline())
     nums_list= list(map(int, sys.stdin.readline().split()))
     ops_list = list(map(int, sys.stdin.readline().split()))
     return [nums_size, nums_list, ops_list]
+
+def custom_div(a, b):
+    if a > 0:
+        return a // b
+    return -(-a//b)
 
 def update_result(val):
     global max, min
@@ -16,31 +22,27 @@ def update_result(val):
     if val > max:
         max = val
 
-def custom_div(a, b):
-    if a > 0:
-        return a // b
-    return -(-a//b)
-
-def dfs(val, idx, nums_list, add, sub, mul, div):
+def dfs(val, idx, add, sub, mul, div):
     if (idx == 0):
         return update_result(val=val)
     
     if add > 0:
-        dfs(val=val+nums_list[-idx], idx=idx-1, nums_list=nums_list
+        dfs(val=val+nums_list[-idx], idx=idx-1
             , add=add-1, sub=sub, mul=mul, div=div)
     if sub > 0:
-        dfs(val=val-nums_list[-idx], idx=idx-1, nums_list=nums_list
+        dfs(val=val-nums_list[-idx], idx=idx-1
             , add=add, sub=sub-1, mul=mul, div=div)
     if mul > 0:
-        dfs(val=val*nums_list[-idx], idx=idx-1, nums_list=nums_list
+        dfs(val=val*nums_list[-idx], idx=idx-1
             , add=add, sub=sub, mul=mul-1, div=div)
     if div > 0:
-        dfs(val=custom_div(val, nums_list[-idx]), idx=idx-1, nums_list=nums_list
+        dfs(val=custom_div(val, nums_list[-idx]), idx=idx-1
             , add=add, sub=sub, mul=mul, div=div-1)
 
 def get_answer():
+    global nums_list
     nums_size, nums_list, ops_list = read_input()
-    dfs(val=nums_list[0], idx=nums_size-1, nums_list=nums_list
+    dfs(val=nums_list[0], idx=nums_size-1
         , add=ops_list[0], sub=ops_list[1], mul=ops_list[2], div=ops_list[3])
     print(max)
     print(min)
