819bd2a447aac7841b6a8ce296d81882c9eef173
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-16 13:34:40 +0900

    [Silver V] Title: 단어 정렬, Time: 64 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,19 +1,10 @@
 import sys
 
-def remove_duplicates(words_list):
-    prev = words_list[0]
-    new_words_list = [prev]
-    for word in words_list:
-        if (prev != word) :
-            new_words_list.append(word)
-            prev = word
-    return new_words_list
-
 def get_answer():
-    words_list = sys.stdin.readlines()[1:]
+    words = sys.stdin.readlines()[1:]
+    words_list = list(set(words))
     words_list.sort()
     words_list.sort(key=lambda word: len(word))
-    words_list = remove_duplicates(words_list)
     sys.stdout.write("".join(words_list))
 
 get_answer()
