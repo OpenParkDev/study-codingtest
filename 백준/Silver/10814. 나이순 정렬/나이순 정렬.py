0e7611a2cdc2f7e172aeeea5040c524b37d4e0bb
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-16 13:39:36 +0900

    [Silver V] Title: 나이순 정렬, Time: 100 ms, Memory: 39740 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,12 +1,8 @@
 import sys
 
-def get_age(member):
-    age, name = member.split()
-    return int(age)
-
 def get_answer():
     members_list = sys.stdin.readlines()[1:]
-    members_list.sort(key=lambda member: get_age(member))
+    members_list.sort(key=lambda member: int(member.split()[0]))
     sys.stdout.write("".join(members_list))
 
 get_answer()
