130a0d62c99ad2e5b06cc23b58acce25d546ac33
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-18 16:13:14 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 68 ms, Memory: 33496 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,29 +1,29 @@
 import sys
 
-def w(input):
+w = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
+for i in range(21):
+    for j in range(21):
+        for k in range(21):
+            if i == 0 or j == 0 or k == 0:
+                w[i][j][k] = 1
+            elif i < j and j < k:
+                w[i][j][k] = w[i][j][k-1] + w[i][j-1][k-1] - w[i][j-1][k]
+            else:
+                w[i][j][k] = w[i-1][j][k] + w[i-1][j-1][k] + w[i-1][j][k-1] - w[i-1][j-1][k-1]
+    
+def w_result(input):
     a, b, c = input
     if a <= 0 or b <= 0 or c <= 0:
         return 1
     elif a > 20 or b > 20 or c > 20:
-        a, b, c = 20, 20, 20
+        return w[20][20][20]
+    else:
+        return w[a][b][c]
 
-    w_result = [[[0 for _ in range(c+1)] for _ in range(b+1)] for _ in range(a+1)]
-    for i in range(a+1):
-        for j in range(b+1):
-            for k in range(c+1):
-                if i == 0 or j == 0 or k == 0:
-                    w_result[i][j][k] = 1
-                elif i < j and j < k:
-                    w_result[i][j][k] = w_result[i][j][k-1] + w_result[i][j-1][k-1] - w_result[i][j-1][k]
-                else:
-                    w_result[i][j][k] = w_result[i-1][j][k] + w_result[i-1][j-1][k] + w_result[i-1][j][k-1] - w_result[i-1][j-1][k-1]
-
-    return w_result[a][b][c]
-    
 def get_answer():
     inputs_list = sys.stdin.read().splitlines()[:-1]
     inputs_list = [tuple(map(int, input.split())) for input in inputs_list]
-    outputs_list = ["w({}, {}, {}) = {}".format(*input, w(input)) for input in inputs_list]
+    outputs_list = ["w({}, {}, {}) = {}".format(*input, w_result(input)) for input in inputs_list]
     sys.stdout.write("\n".join(outputs_list))
 
 get_answer()
