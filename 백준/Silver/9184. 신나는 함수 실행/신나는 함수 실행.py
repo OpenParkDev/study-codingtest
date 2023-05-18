6581dcb74746eb06693252e65d921e807d0a4573
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-18 16:31:58 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 68 ms, Memory: 33496 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,29 +1,25 @@
 import sys
 
-w = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
-for i in range(21):
-    for j in range(21):
-        for k in range(21):
-            if i == 0 or j == 0 or k == 0:
-                w[i][j][k] = 1
-            elif i < j and j < k:
-                w[i][j][k] = w[i][j][k-1] + w[i][j-1][k-1] - w[i][j-1][k]
-            else:
-                w[i][j][k] = w[i-1][j][k] + w[i-1][j-1][k] + w[i-1][j][k-1] - w[i-1][j-1][k-1]
-    
-def w_result(input):
-    a, b, c = input
+w_result = [[[None for _ in range(21)] for _ in range(21)] for _ in range(21)]
+            
+def w(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return 1
     elif a > 20 or b > 20 or c > 20:
-        return w[20][20][20]
-    else:
-        return w[a][b][c]
+        return w(20, 20, 20)
+    
+    if w_result[a][b][c] == None:
+        if a < b and b < c:
+            w_result[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        else:
+            w_result[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    
+    return w_result[a][b][c]
 
 def get_answer():
     inputs_list = sys.stdin.read().splitlines()[:-1]
     inputs_list = [tuple(map(int, input.split())) for input in inputs_list]
-    outputs_list = ["w({}, {}, {}) = {}".format(*input, w_result(input)) for input in inputs_list]
+    outputs_list = ["w({}, {}, {}) = {}".format(*input, w(*input)) for input in inputs_list]
     sys.stdout.write("\n".join(outputs_list))
 
 get_answer()
