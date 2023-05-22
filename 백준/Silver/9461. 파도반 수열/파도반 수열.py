4657402939e105d0a0c2fe2d098098ba7b539afe
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-22 15:12:10 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -3,16 +3,13 @@ import sys
 p_result = [0, 1, 1, 1, 2, 2]
             
 def p(n):
-    tail = len(p_result)-1
-    if n > tail:
-        for i in range(tail+1, n+1):
-            p_result.append(p_result[i-1] + p_result[i-5])
+    for _ in range(n-len(p_result)+1):
+            p_result.append(p_result[-1] + p_result[-5])     
     return p_result[n]
 
 def get_answer():
-    inputs_size = int(sys.stdin.readline())
-    inputs_list = list(map(int, sys.stdin.readlines()))
-    outputs_list = [str(p(i)) for i in inputs_list]
+    inputs_list = sys.stdin.readlines()[1:]
+    outputs_list = [str(p(int(i))) for i in inputs_list]
     sys.stdout.write("\n".join(outputs_list))
 
 get_answer()
