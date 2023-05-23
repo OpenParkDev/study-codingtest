fbdb75e45fb41c243f37a86c10e1d53b38f52c43
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-23 16:45:41 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,21 +2,15 @@ import sys
             
 def get_max_sum(nums_list):
     max_sum = -1000
-    sums_list = []
-    for i in range(len(nums_list)):
-        num = int(nums_list[i])
-        if i == 0:
-            sums_list.append(num)
-        else:
-            sums_list.append(max(sums_list[i-1]+num, num))
-        
-        curr = sums_list[-1]
-        if curr > max_sum:
-            max_sum = curr
+    sums_list = [0]
+    for num in nums_list:
+        sums_list.append(max(sums_list[-1]+num, num))
+        if sums_list[-1] > max_sum:
+            max_sum = sums_list[-1]
     return max_sum
 
 def get_answer():
-    inputs_list = sys.stdin.readlines()[1].split()
+    inputs_list = list(map(int, sys.stdin.readlines()[1].split()))
     print(get_max_sum(inputs_list))
 
 get_answer()
