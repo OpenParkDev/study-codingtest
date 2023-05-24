9997c0d417d6d57e77b23c2804dc18928b888631
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-24 15:10:30 +0900

    [Silver III] Title: 1로 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,8 +1,8 @@
 d = {1:0, 2:1, 3:1}
 def a(n):
-    if n not in d:
-        val = min(a(n//3)+n%3, a(n//2)+n%2) + 1
-        d[n] = val
-    return d[n]
-
+    if n in d:
+        return d[n]
+    val = min(a(n//3)+n%3, a(n//2)+n%2) + 1
+    d[n] = val
+    return val
 print(a(int(input())))
