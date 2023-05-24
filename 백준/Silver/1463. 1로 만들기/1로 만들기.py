f11dacd9c64e0b2c89c2835de271008e5fd7035b
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-05-24 15:05:56 +0900

    [Silver III] Title: 1로 만들기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,8 @@
-def get_min_cal(num):
-    c = [0, 0, 1, 1]
-    for i in range(4, num+1):
-        if i % 3 == 0 and i % 2 == 0:
-            c.append(min(c[i//3]+1, c[i//2]+1))
-        elif i % 3 == 0:
-            c.append(min(c[i//3]+1, c[i-1]+1))
-        elif i % 2 == 0:
-            c.append(min(c[i//2]+1, c[i-1]+1))
-        else:
-            c.append(c[i-1]+1)
-    return c[num]
+def a(n):
+    d = {1:0, 2:1, 3:1}
+    if n not in d:
+        val = min(a(n//3)+n%3, a(n//2)+n%2) + 1
+        d[n] = val
+    return d[n]
 
-def get_answer():
-    num = int(input())
-    print(get_min_cal(num))
-
-get_answer()
+print(a(int(input())))
