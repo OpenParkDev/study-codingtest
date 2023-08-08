d8b83eb5b5dc7284c75ae3a096616c4b91d52829
openparkdev <124948276+OpenParkDev@users.noreply.github.com>
2023-08-08 15:56:42 +0900

    [Gold IV] Title: 오큰수, Time: 1224 ms, Memory: 265932 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,17 @@
+import sys
+
+input_size = int(sys.stdin.readline())
+input_nums = list(map(int, sys.stdin.readline().split()))
+stack = []
+answer = [-1] * input_size
+for idx in range(input_size):
+    num = input_nums[idx]
+    while len(stack) != 0:
+        top = stack[-1]
+        if top[0] < num:
+            answer[top[1]] = num
+            stack.pop()
+        else:
+            break
+    stack.append((num, idx)) 
+print(' '.join(map(str, answer)))
